7f4796d632cbe9598151aedd495a9dd5ac16c0e3
Kevin MacDonald <macdkw@gmail.com>
2022-08-14 17:11:00 -0500

    Progress

--- scraping.py
+++ scraping.py
@@ -2,26 +2,35 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
-from webdriver_manager.chrome import ChromeDriverManager
 import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=True)
+    browser = Browser("chrome", **executable_path, headless=True)
+
     news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary
     data = {
-      "news_title": news_title,
-      "news_paragraph": news_paragraph,
-      "featured_image": featured_image(browser),
-      "facts": mars_facts(),
-      "last_modified": dt.datetime.now()}
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "hemispheres": hemispheres(browser),
+        "last_modified": dt.datetime.now()
+
+    }
+
     # Stop webdriver and return data
     browser.quit()
     return data
 
 def mars_news(browser):
-    # Visit the Mars news site
+
+    # Scrape Mars News
+    # Visit the mars nasa news site
     url = 'https://redplanetscience.com/'
     browser.visit(url)
 
@@ -31,6 +40,8 @@ def mars_news(browser):
     # Convert the browser html to a soup object and then quit the browser
     html = browser.html
     news_soup = soup(html, 'html.parser')
+
+    # Add try/except for error handling
     try:
         slide_elem = news_soup.select_one('div.list_text')
         # Use the parent element to find the first 'a' tag and save it as 'news_title'
@@ -42,11 +53,6 @@ def mars_news(browser):
         return None, None
 
     return news_title, news_p
-
-
-
-
-# ## JPL Space Images Featured Image
 
 def featured_image(browser):
     # Visit URL
@@ -63,7 +69,7 @@ def featured_image(browser):
 
     # Add try/except for error handling
     try:
-        # Find the relative image url
+        # find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 
     except AttributeError:
@@ -90,6 +96,23 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    hemisphere_image_urls = []
+    links = browser.find_by_css('.product-item img')
+    for i in range(len(links)):
+        hemisphere = {}
+        browser.find_by_css('.product-item img')[i].click()
+        sample_elem = browser.links.find_by_text('Sample').first
+        hemisphere['img_url'] = sample_elem['href']
+        hemisphere['title'] = browser.find_by_css('h2.title').text
+        hemisphere_image_urls.append(hemisphere)
+        browser.back()
+
+    return hemisphere_image_urls
+
 if __name__ == "__main__":
+
     # If running as script, print scraped data
     print(scrape_all())
